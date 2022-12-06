8f42cfa39b96bde5cec30ef5c95aaf913ae41bd2
Marcio-Brust <brustmw21@gmail.com>
2022-12-06 02:00:43 -0300

    atualizacao

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,22 @@ import React from "react";
 import style from "./Header.module.css";
 
 const Header = () => {
+  function handleClick({ target }) {
+    const body = document.querySelector("body");
+    body.classList.toggle("light");
+    if (body.classList.contains("light")) {
+      target.innerHTML = "Dark";
+    } else {
+      target.innerHTML = "Light";
+    }
+  }
+
   return (
     <header className={style.header}>
       <h1 className={style.h1}>Márcio Brust</h1>
-      <button className={style.btn}>Light</button>
+      <button className={style.btn} onClick={handleClick}>
+        Light
+      </button>
     </header>
   );
 };
--- src/Components/Projetos.js
+++ src/Components/Projetos.js
@@ -10,6 +10,27 @@ import foto6 from "../assets/img/artista-6.png";
 const fotos = [foto1, foto2, foto3, foto4, foto5, foto6];
 
 const Projetos = () => {
+  class ScrollShadow {
+    constructor(projetos) {
+      this.projetos = document.querySelectorAll(projetos);
+      this.addScrollEvent = this.addScrollEvent.bind(this);
+      this.activeLinkScroll();
+      this.addScrollEvent();
+    }
+    addScrollEvent() {
+      this.projetos.forEach((projeto) => {
+        projeto =
+          window.pageYOffset > projeto.offsetTop - window.innerHeight * 0.9
+            ? projeto.classList.add("active")
+            : projeto.classList.remove("active");
+      });
+    }
+    activeLinkScroll() {
+      window.addEventListener("scroll", this.addScrollEvent);
+    }
+  }
+
+  const scroll = new ScrollShadow("div");
   return (
     <section className={style.containerProjeto}>
       {fotos.map((foto, index) => (
